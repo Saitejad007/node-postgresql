63318b29bbd8a311be0c097dfbc600c4e0531f57
Sai Teja <saitejadevineni@gmail.com>
2024-01-27 21:09:56 +0530

    updated error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,20 +9,25 @@ const register = async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
     const salt = await bcrypt.genSalt(10);
     const hashedPwd = await bcrypt.hash(password, salt);
-    const user = await User.create({
-      first_name: firstName,
-      last_name: lastName,
-      email: email,
-      password: hashedPwd,
-    });
-    if (user) {
+
+    const existingUser = await User.findOne({ where: { email: email } });
+    if (existingUser) {
+      res.status(409).send({
+        message: "User with this email already exists",
+        status: 409,
+      });
+    } else {
+      const user = await User.create({
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        password: hashedPwd,
+      });
       const token = jwt.sign({ id: user.id }, process.env.SECRET, {
         expiresIn: 60 * 60 * 1000,
       });
       res.cookie("jwt", token, { httpOnly: true, maxAge: 60 * 60 * 1000 });
-      res.status(201).json(user);
-    } else {
-      res.status(409).send("User already exists");
+      res.status(201).json({ user });
     }
   } catch (error) {
     res.status(500).send(error);
@@ -42,12 +47,17 @@ const login = async (req, res) => {
         });
         res.cookie("jwt", token, { httpOnly: true, maxAge: 60 * 60 * 1000 });
         const { password, ...others } = user.dataValues;
-        res.status(201).json(others);
+        res.status(200).json(others);
       } else {
-        res.status(401).send("Wrong Password");
+        res.status(401).send({
+          message: "Invalid credentials",
+          status: 401,
+        });
       }
     } else {
-      res.status(404).send("User not found");
+      res
+        .status(404)
+        .send({ message: "User with this email does not exist", status: 404 });
     }
   } catch (error) {
     res.status(500).send(error);
